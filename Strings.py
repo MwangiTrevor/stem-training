8e7a55741f2c37f9aba2b6fc0e66bd52a7369767
MwangiTrevor <105647967+MwangiTrevor@users.noreply.github.com>
2022-05-20 01:24:21 -0700

    Add files via upload

---
+++ Strings.py
@@ -0,0 +1,32 @@
+name= "Trevor Mwangi"
+
+#Multiple line strings
+msg= """Q239803 you have done somthing """
+
+
+print(msg)
+city='nairobi'
+#upper()to convert to upper case
+print( city.upper())
+uni = "JKUAT"
+print (uni.lower())
+fruit="apple"
+print(fruit[:2])
+print(fruit[:1])
+print(fruit[-1])
+print(fruit[-2])
+f_name=" Trevor Mwangi"
+print(f_name.strip())
+f_name="Trevor"
+s_name="Mwangi"
+full_name =f_name+s_name
+city="Lamu"
+print(len(city))
+name="Abrakadabra"
+print(len(city))
+x=100 inter
+y=3.14
+z=1
+print(str(x))
+print(int(Y))#int
+print(float(Z))
---
+++ calc.py
@@ -0,0 +1,5 @@
+f_num=input("Enter the first number :")
+s_num("Enter the second number :")
+sum_of_nums=f_num + s_num
+print("The sum of thee 2 nums is"+str(sum_of_nums))
+int (f_num)+int (s_num)
